af64c282358c803dfc1990c228db0c29c5aa5912
Black-Kim <70114446+Black-Kim@users.noreply.github.com>
2020-09-02 18:16:52 +0900

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -8,7 +8,7 @@ public class SimpleCalculator {
 		//두개의 숫자와 연산자를 입력받아 연산결과를 알려주는 프로그램을 만들어주세요.
 		
 		Scanner sc = new Scanner(System.in);
-		int num1 , num2;
+		double num1 , num2;
 
 		
 		System.out.println("두개의 숫자를 계산해 주는 계산기 입니다.\n"+"첫번째 숫자를 적으세요>");
@@ -19,10 +19,10 @@ public class SimpleCalculator {
 		String a = sc.nextLine();
 		
 		
-		int cal = a.equals("+") ? num1 + num2 : 
+		double cal = a.equals("+") ? num1 + num2 : 
 				(a.equals("-")? num1 - num2 :
 				(a.equals("*")? num1 * num2 : 
-				(a.equals("/")? Math.round((num1 / num2)*10.0)/10.0 : 0)));
+				(a.equals("/")? Math.round((num1 / num2)*10.0)/10.0: 0)));
 	    System.out.println("계산된 값은 "+cal+"입니다.");
 		
 		
